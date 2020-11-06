ca2725bf1fec202be448d75c0193b7338ba5e306
lzq <1763140724@qq.com>
2020-11-06 17:35:36 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -17,6 +17,8 @@ module.exports = {
         // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
         // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
         repoLabel: '查看源码',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
         // 假如文档放在一个特定的分支下：
         docsBranch: 'master',
         editLinks: true,
